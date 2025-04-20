d4a6059fe8d917fe6122b3312f6814332c659f43
figurann <slashlemon@gmail.com>
2025-04-20 22:29:52 +0700

    (+) Menambahkan

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -11,10 +12,13 @@ class UserController extends Controller
     {
         // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
         ];
 
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data); // menambah data user baru
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
