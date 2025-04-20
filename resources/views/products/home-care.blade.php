7b3ce76eea4a86e84ecea87135d92876d374ba03
figurann <slashlemon@gmail.com>
2025-04-20 14:43:16 +0700

    (+) Menambahkan Views dan Controllers

---
+++ resources/views/products/home-care.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Care Products</title>
+</head>
+
+<body>
+    <h1>Home Care Products</h1>
+    <p>Discover a variety of products to care for your home and family.</p>
+</body>
+
+</html>
