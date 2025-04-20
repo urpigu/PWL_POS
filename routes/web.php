36215bca6f41bdca4cc273b48dccec9174d6a087
figurann <slashlemon@gmail.com>
2025-04-20 14:18:41 +0700

    (+) Menambahkan Route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TransactionController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Halaman Home
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// Halaman Products
+Route::prefix('category')->group(function () {
+    Route::get('food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('home-care', [ProductController::class, 'homeCare']);
+    Route::get('baby-kid', [ProductController::class, 'babyKid']);
+});
+
+// Halaman User
+Route::get('user/{id}/name/{name}', [UserController::class, 'show']);
+
+// Halaman Penjualan
+Route::get('penjualan', [TransactionController::class, 'index']);
