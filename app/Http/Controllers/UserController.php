395ae0e8044840f25c632759eb4162be9bbb5583
figurann <slashlemon@gmail.com>
2025-04-21 15:41:10 +0700

    (+) Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,18 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // Menghitung jumlah user dengan level_id = 2
-        $userCount = UserModel::where('level_id', 2)->count();
+        $user = UserModel::firstOrNew(
+            ['username' => 'manager33'],
+            [
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2,
+            ]
+        );
 
-        // Mengambil semua data user dengan level_id = 2
-        $data = UserModel::where('level_id', 2)->get();
+        $user->save();
 
-        // Mengirim data user dan jumlah user ke view
-        return view('user', ['userCount' => $userCount, 'data' => $data]);
+        return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,35 +8,19 @@
 <body>
     <h1>Data User</h1>
 
-    <!-- Menampilkan jumlah pengguna -->
-    <h3>Jumlah Pengguna</h3>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
         </tr>
         <tr>
-            <td>{{ $userCount }}</td> <!-- Menampilkan jumlah pengguna -->
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
-    </table>
-
-    <!-- Tabel data pengguna -->
-    <h3>Daftar Pengguna</h3>
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
-        </tr>
-        @foreach ($data as $user)
-            <!-- Loop untuk menampilkan setiap pengguna -->
-            <tr>
-                <td>{{ $user->user_id }}</td>
-                <td>{{ $user->username }}</td>
-                <td>{{ $user->nama }}</td>
-                <td>{{ $user->level_id }}</td>
-            </tr>
-        @endforeach
     </table>
 </body>
 
