23fc57288c47f93689cd04ab62c83db98022d315
urpigu <urpigu>
2025-05-24 18:59:14 +0700

    JS10-Prak4

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register',App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+Route::get('levels', [LevelController:: class, 'index']);
+Route::post('levels', [LevelController:: class, 'store' ]);
+Route::get('levels/{level}', [LevelController:: class, 'show' ]);
+Route::put('levels/{level}', [LevelController::class, 'update' ]);
+Route::delete('levels/{level}', [LevelController:: class, 'destroy']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
